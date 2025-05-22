bdbd9adf1c78e68b5a0050fade98895e780b22b8
Siddharth Sharma <siddharth1119sid@gmail.com>
2025-05-22 17:57:14 +0530

    qwertyu

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./HTML/**/*.{html,js}",
+    "./html2/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
